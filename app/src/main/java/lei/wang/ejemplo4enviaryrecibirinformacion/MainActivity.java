5efdc65bdcaf7d941aa8b0d83c37e46acd947f09
leiwang <lei.wang@cieep.com>
2023-09-22 10:09:57 +0200

    Initial commit

---
+++ app/src/main/java/lei/wang/ejemplo4enviaryrecibirinformacion/MainActivity.java
@@ -0,0 +1,26 @@
+package lei.wang.ejemplo4enviaryrecibirinformacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText txtPassword;
+    private Button btnDesencriptar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        inicializarVista();
+    }
+
+    private void inicializarVista() {
+        txtPassword = findViewById(R.id.txtPasswordMain);
+        btnDesencriptar = findViewById(R.id.btnDesencriptarMain);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ejemplo 4 Enviar y Recibir informacion"
+include(":app")
